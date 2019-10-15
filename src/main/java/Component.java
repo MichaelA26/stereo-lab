a57c68f891d2726f6072235782902c3c66a6ab7f
Michael Alexander <michael_alexander_20@outlook.com>
2019-10-15 12:05:58 +0100

    initial commit and created component class

---
+++ src/main/java/Component.java
@@ -0,0 +1,13 @@
+public abstract class Component {
+    protected int volume;
+
+    public Component(){
+        this.volume = 0;
+    }
+
+
+    public int getVolume(){
+        return volume;
+    }
+    
+}
